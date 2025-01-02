260f24ec9bd884e5e3c069eb6f48ee027a69a25e
JiHo Jeon <wjswlgh96@naver.com>
2025-01-02 14:06:51 +0900

    fix: Lv.1 수정

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -1,42 +1,59 @@
 package com.example.calculator;
 
-import java.util.List;
 import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Calculator calculator = new Calculator();
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
+            int x = sc.nextInt();
             sc.nextLine();
 
             System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            int y = sc.nextInt();
             sc.nextLine();
 
             System.out.print("사칙연산 기호를 입력하세요: ");
             String operator = sc.nextLine();
 
-            Integer result = calculator.calculate(num1, num2, operator);
-            if(result == null) continue;
+            int result = 0;
+            switch (operator.charAt(0)) {
+                case '+' : {
+                    result = x + y;
+                    break;
+                }
+                case '-': {
+                    result = x - y;
+                    break;
+                }
+                case '*': {
+                    result = x * y;
+                    break;
+                }
+                case '/': {
+                    if (y == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
 
-            System.out.println();
+                    result = x / y;
+                    break;
+                }
+                default: {
+                    System.out.println("잘못된 기호를 입력하셨습니다. 처음으로 돌아갑니다.");
+                    break;
+                }
+            }
 
             System.out.println("결과: " + result);
-
-            System.out.println("현재까지의 계산 과정: " + calculator.printAllResult());
-
-            System.out.println();
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             System.out.println("아무키나 눌러주시면 더 계산합니다.");
             if(sc.nextLine().equals("exit")) {
                 break;
-            }
+            };
         }
     }
 }
